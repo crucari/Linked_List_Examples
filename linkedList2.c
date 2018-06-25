738512673931e6ee514cf07a5d55c399a533be58
Carina Cruz <lecruz@bu.edu>
2018-06-25 23:46:22 +0000

    update ex 2

--- linkedList2.c
+++ linkedList2.c
@@ -24,17 +24,34 @@ namespace LinkedList
                 headNode.AddToEnd(data);
             }
             }
-        }
-    }
+
+            public void Print()
+            {
+                if (headNode != null)
+                {
+                    headNode.Print();
+                }
+
+            }
 
     class Program
     {
         static void Main(string[] args)
         {
-             Node myNode = new Node(7);  //creating a node
-             myNode.AddToEnd(5);   //adding nodes to the list
+            MyList list = new MyList();
+             myNode.AddToEnd(9);
+             myNode.AddToEnd(5);
+             myNode.AddToEnd(7);
              myNode.AddToEnd(11);
-             myNode.AddToEnd(4);
-             myNode.Print();
+             list.Print();
+
+        }
+
+
+            //  Node myNode = new Node(7);  //creating a node
+            //  myNode.AddToEnd(5);   //adding nodes to the list
+            //  myNode.AddToEnd(11);
+            //  myNode.AddToEnd(4);
+            //  myNode.Print();
         }
     }
